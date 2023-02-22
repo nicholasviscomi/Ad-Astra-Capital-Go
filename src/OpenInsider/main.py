bafc19e09835c1554a44b107d0eb9a273dc44da4
Nick Viscomi <nicholasviscomi@gmail.com>
2023-02-22 08:24:10 -0500

    failed to scrape because SEC blocks the website

--- src/OpenInsider/main.py
+++ src/OpenInsider/main.py
@@ -2,43 +2,30 @@ import requests
 from bs4 import BeautifulSoup
 from dataclasses import dataclass
 
+# might just search openinsider.com for a ton of data, download the website and then scrape the links. 
+# I could do the same with EDGAR I think?
+# here's where to check the price change of the ticker: https://www.nasdaq.com/market-activity/stocks/aapl/historical
 
-
-@dataclass
-class Asset:
-    filing_date: str
-    trade_date: str
-    ticker: str
-    price: float
-    qty_bought: int
+URL_BASE = "https://www.sec.gov" # base of the url to be added before each link that is scraped
+form_links = []
 
 if __name__ == "__main__":
-    URL = "http://openinsider.com/"
-    page = requests.get(URL)
+    soup = BeautifulSoup(open("/Users/nickviscomi/Desktop/VSCode Projects/Python/AssetTradingAnalysis/src/OpenInsider/Assets/1.html", encoding="utf8"), "html.parser")
+    
+    table = soup.find("body").find_all("table")[6]
 
-    soup = BeautifulSoup(page.content, "html.parser")
+    for i, row in enumerate(table.find_all("tr")):
+        if i == 0 or i % 2 != 0: continue # don't want the first one and only want evens
+        link = row.find_all("td")[1].find("a")["href"]
+        form_links.append(URL_BASE + link)
 
-    table = soup.find("tbody")
-    rows = table.find_all("tr")
+    form = requests.get(form_links[0])
+    soup = BeautifulSoup(form.content, "html.parser")
+    print(soup.prettify())
 
-    assets = []
+    
+    
 
-    for row in rows:
-        tds = row.find_all("td")
-        asset = Asset("", "", "", -1, -1)
-        for i, td in enumerate(tds):
-            text = td.get_text()
-            if i == 1:
-                asset.filing_date = text
-            if i == 2:
-                asset.trade_date = text
-            if i == 3:
-                asset.ticker = text
-            if i == 8:
-                asset.price = text
-            if i == 9:
-                asset.qty_bought = text
-        assets.append(asset)
+    
 
-    for a in assets:
-        print(f"{a.ticker} @ {a.price}")
+
